fbc84c92a522a49e90f7e320ddbf676df7128e1b
k1mjunyoung <me@kimjunyoung.com>
2026-03-17 16:35:45 +0900

    1.9 조건문

---
+++ section01/chapter09.js
@@ -0,0 +1,24 @@
+// Switch
+// 다수의 조건을 처리할 때 if보다 더 직관적
+let animal = "owl";
+switch (animal) {
+  case "cat": {
+    console.log("고양이");
+    break;
+  }
+  case "dog": {
+    console.log("강아지");
+    break;
+  }
+  case "bear": {
+    console.log("곰");
+    break;
+  }
+  default: {
+    console.log("No animal");
+  }
+  case "bear": {
+    console.log("곰");
+    break;
+  }
+}
