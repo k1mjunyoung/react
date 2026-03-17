0c951028683d19c1b0925ea90a5f9580a20b8a49
k1mjunyoung <me@kimjunyoung.com>
2026-03-17 14:30:16 +0900

    1.5 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// Number
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mIng = -Infinity;
+
+let nan = NaN;
+console.log(1 * "hello");
+
+// String
+let myName = "hello";
+let myLocation = "Korea";
+let introduce = myName + myLocation;
+console.log(introduce);
+
+let introduceText = `${myName} ${myLocation}에 거주합니다`; // 템플릿 리터럴
+console.log(introduceText);
+
+// Null
+let empty = null;
+
+// Undefined
+let none;
+console.log(none);
