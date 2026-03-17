a1834f042763c5cab2a220176030aaceba4ecb16
k1mjunyoung <me@kimjunyoung.com>
2026-03-17 10:20:32 +0900

    1.4 변수와 상수

---
+++ section01/chapter04.js
@@ -0,0 +1,22 @@
+console.log("chapter 4");
+
+// 변수
+let age = 27;
+age = 30;
+console.log("age: " + age);
+
+// 상수
+const birth = "1101";
+// birth = "1231";
+console.log(birth);
+
+// 변수 네이밍
+// 1. $, _ 제외한 기호는 사용할 수 없다.
+// 2. 숫자로 시작할 수 없다.
+// 에약어를 사용할 수 없다.
+let $_name;
+
+// 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
