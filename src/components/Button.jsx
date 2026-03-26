ffc6fbb19cea991143817fb182ca2adf26fbebec
k1mjunyoung <me@kimjunyoung.com>
2026-03-26 10:46:12 +0900

    5.5 이벤트 핸들러

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 function Button({ children, text, color = "black"}) {
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e); // SyntheicBaseEvent: 합성 이벤트 객체 -> 크로스 브라우저 이슈를 해결하이 위해 만들어짐
+    console.log(text);
+  }
+
   return (
-    <button style={{color: color}}>
+    <button 
+      onClick={onClickButton} // onClickButton()이 아닌 함수의 이름을 전달
+      // onMouseEnter={onClickButton}
+      style={{color: color}}>
       {text} - {color}
       {children}
     </button>
