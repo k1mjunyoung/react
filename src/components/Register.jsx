83ea8dd7e2e4b13f3ca66933b090bb69fbbf8a46
k1mjunyoung <me@kimjunyoung.com>
2026-03-26 15:35:13 +0900

    5.9 State 사용자 입력 관리 2

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,39 +1,35 @@
 import { useState } from "react";
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBitrh] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  }
+  const [input, setInput ] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio:"",
+  })
 
-  const onChangeBirth = (e) => {
-    setBitrh(e.target.value);
-  }
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  }
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  }
+  // 통합 이벤트 핸들러
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value, // JavaScript에서 객체 리터럴의 키를 동적으로 지정할 때 []를 사용 
+    });
+  };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
       </div>
 
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" /> 
+        <input name="birth" value={input.birth} onChange={onChange} type="date" /> 
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value="" />
           <option value="kr">한국</option>
           <option value="us">일본</option>
@@ -42,8 +38,7 @@ const Register = () => {
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio}/>
-        {bio}
+        <textarea name="bio" value={input.bio} onChange={onChange}/>
       </div>
     </div>
   )
