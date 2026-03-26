a3faea9aeeb85f3aec16f50e5e2fd2e08612f049
k1mjunyoung <me@kimjunyoung.com>
2026-03-26 17:34:57 +0900

    6.3 카운터 앱 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Cotroller'
+import {useState} from 'react'
 
 function App() { // App 컴포넌트(부모 컴포넌트)
+  const [count, setCount] = useState(0);
+  
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
 
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
 
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   )
