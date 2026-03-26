b571969842cec964ec7855b9c7076280c61044bb
k1mjunyoung <me@kimjunyoung.com>
2026-03-26 10:00:20 +0900

    5.4 Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Button from './components/Button'
 
 function App() { // App 컴포넌트(부모 컴포넌트)
+  const buttonProps = {
+    text : "매일",
+    color: "red"
+  };
+
   return (
     <>
       <Header />
+      <Button {...buttonProps}/>
+      <Button text={"카페"}/>
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+        <Header/>
+      </Button>
       <Main />
       <Footer />
     </>
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+function Button({ children, text, color = "black"}) {
+  return (
+    <button style={{color: color}}>
+      {text} - {color}
+      {children}
+    </button>
+  )
+};
+
+export default Button;
